e712efdb2d5e7ee78e5a7fde06a4470f55198fff
Gabriel Sinapsis <gaby@sinapsissl.com>
2022-02-06 15:48:20 -0300

    Se agrego bloques try catch que faltaban. Se agrego loggers. Se agrego controladores ingresarArticuloOrdenGet, ingresarArticuloOrdenBuscar y ingresarArticuloOrdenConfirmar

--- controllers/urbanoControllers.js
+++ controllers/urbanoControllers.js
@@ -80,14 +80,18 @@ function agregarCodigoTecnico(arrayTickets) {
 
 //Obtener Orden de reparacion y sus articulos con precios
 exports.ordenDeReparacion = async (req, res) => {
-  const host = req.connection.remoteAddress;
-  const numberOrder = req.query.orden;
-  const queryOrden = `SELECT * FROM trabajos WHERE nrocompro LIKE "ORX0011000${numberOrder}"`;
-  const queryCotizacionDolar = `SELECT * FROM cotiza  WHERE codigo =  "BD"`;
-  const queryArticulosEnOrden = `SELECT * FROM trrenglo INNER JOIN articulo ON trrenglo.codart= articulo.codigo
-                                   WHERE trrenglo.nrocompro LIKE  "ORX0011000${numberOrder}"`;
-
   try {
+    const codigo_tecnico = req.body.codigo_tecnico;
+    const host = req.body.host;
+    logger.info(
+      `ordenDeReparacion - Usuario: ${codigo_tecnico} - Host: ${host}`
+    );
+
+    const numberOrder = req.query.orden;
+    const queryOrden = `SELECT * FROM trabajos WHERE nrocompro LIKE "ORX0011000${numberOrder}"`;
+    const queryCotizacionDolar = `SELECT * FROM cotiza  WHERE codigo =  "BD"`;
+    const queryArticulosEnOrden = `SELECT * FROM trrenglo INNER JOIN articulo ON trrenglo.codart= articulo.codigo
+                                     WHERE trrenglo.nrocompro LIKE  "ORX0011000${numberOrder}"`;
     //Buscar Orden de trabajo
     let orden = await getFromUrbano(queryOrden);
     if (!orden[0]) {
@@ -132,20 +136,25 @@ exports.ordenDeReparacion = async (req, res) => {
       }
     }
   } catch (error) {
-    // console.log(error.message);
-    logger.error(`ordenDeReparacion - Host: ${host} - error: ${error.message}`);
+    logger.error(
+      `ordenDeReparacion - Usuario: ${codigo_tecnico} - Error: ${error.message} - Host: ${host}`
+    );
   }
 };
 
 //Ordenes Pendientes del taller segun sector
 exports.ordenesPendientes = async (req, res) => {
-  const host = req.connection.remoteAddress;
-  const sector = req.params.sector;
-  const queryOrdenesPendientes = `SELECT * FROM trabajos 
-                  WHERE  codiart = ".${sector}" AND estado = 21 AND codigo != "ANULADO"
-                  ORDER BY prioridad DESC`;
-
   try {
+    const sector = req.params.sector;
+    const codigo_tecnico = req.body.codigo_tecnico;
+    const host = req.body.host;
+    logger.info(
+      `ordenesPendientes - Sector: ${sector} - Usuario: ${codigo_tecnico} - Host: ${host}`
+    );
+
+    const queryOrdenesPendientes = `SELECT * FROM trabajos 
+                    WHERE  codiart = ".${sector}" AND estado = 21 AND codigo != "ANULADO"
+                    ORDER BY prioridad DESC`;
     let ordenesPendientes = await getFromUrbano(queryOrdenesPendientes);
     ordenesPendientes = await getOrdenesFormateadas(ordenesPendientes);
 
@@ -164,16 +173,22 @@ exports.ordenesPendientes = async (req, res) => {
       fila: 0,
     });
   } catch (error) {
-    logger.error(`ordenesPendientes - Host: ${host} - error: ${error.message}`);
+    logger.error(
+      `ordenesPendientes - Usuario: ${codigo_tecnico} - Error: ${error.message} - Host: ${host}`
+    );
   }
 };
 
 //Ordenes en proceso
 exports.ordenesEnProceso = async (req, res) => {
-  const host = req.connection.remoteAddress;
-  const queryOrdenesEnProceso = `SELECT * FROM trabajos WHERE estado = 22 ORDER BY tecnico`;
-
   try {
+    const codigo_tecnico = req.body.codigo_tecnico;
+    const host = req.body.host;
+    logger.info(
+      `ordenesEnProceso - Usuario: ${codigo_tecnico} - Host: ${host}`
+    );
+
+    const queryOrdenesEnProceso = `SELECT * FROM trabajos WHERE estado = 22 ORDER BY tecnico`;
     let ordenesEnProceso = await getFromUrbano(queryOrdenesEnProceso);
     ordenesEnProceso = await getOrdenesFormateadas(ordenesEnProceso);
 
@@ -192,7 +207,9 @@ exports.ordenesEnProceso = async (req, res) => {
       fila: 0,
     });
   } catch (error) {
-    logger.error(`ordenesEnProceso - Host: ${host} - error: ${error.message}`);
+    logger.error(
+      `ordenesEnProceso - Usuario: ${codigo_tecnico} - Error: ${error.message} - Host: ${host}`
+    );
   }
 };
 
@@ -220,7 +237,11 @@ class Estadisticas {
 
 exports.estadisticasTecnicos = async (req, res) => {
   try {
-    const host = req.connection.remoteAddress;
+    const codigo_tecnico = req.body.codigo_tecnico;
+    const host = req.body.host;
+    logger.info(
+      `estadisticasTecnicos - Usuario: ${codigo_tecnico} - Host: ${host}`
+    );
 
     if (req.query.desde && req.query.hasta) {
       let desde, hasta;
@@ -325,7 +346,224 @@ exports.estadisticasTecnicos = async (req, res) => {
     }
   } catch (error) {
     logger.error(
-      `estadisticasTecnicos - Host: ${host} - error: ${error.message}`
+      `estadisticasTecnicos - Usuario: ${codigo_tecnico} - Error: ${error.message} - Host: ${host}`
+    );
+  }
+};
+
+//Ingresar Articulo en Orden
+const form = {
+  mensaje: "",
+  error: "",
+  ordenReparacion: "",
+  codigoArticulo: "",
+  numeroSerie: "",
+  orden: "",
+  articulo: "",
+  tecnicos: constantes.tecnicos,
+};
+
+exports.ingresarArticuloOrdenGet = async (req, res) => {
+  try {
+    const codigo_tecnico = req.body.codigo_tecnico;
+    const host = req.body.host;
+    logger.info(
+      `ingresarArticuloOrdenGet - Usuario: ${codigo_tecnico} - Host: ${host}`
+    );
+
+    form.mensaje = "GET";
+    form.error = "";
+    form.ordenReparacion = "";
+    form.codigoArticulo = "";
+    form.numeroSerie = "";
+    form.orden = "";
+    form.articulo = "";
+    res.render("ingresarArticuloOrden", {
+      usuario: { nombre: codigo_tecnico },
+      titulo: "Ingresar Articulo",
+      form: form,
+    });
+  } catch (error) {
+    logger.error(
+      `ingresarArticuloOrdenGet - Usuario: ${codigo_tecnico} - Error: ${error.message} - Host: ${host}`
+    );
+  }
+};
+
+exports.ingresarArticuloOrdenBuscar = async (req, res) => {
+  try {
+    const host = req.body.host;
+    const codigo_tecnico = req.body.codigo_tecnico;
+    const tecnico = req.body.tecnico;
+    const orden_reparacion = req.body.ordenReparacion;
+    const codigo_articulo = req.body.codigoArticulo;
+    const numero_serie = req.body.numeroSerie;
+
+    logger.info(
+      `ingresarArticuloOrdenBuscar - Usuario: ${codigo_tecnico} - Host: ${host} 
+      Tecnico: ${tecnico}
+      Orden: ${orden_reparacion}
+      Codigo Articulo: ${codigo_articulo}
+      Numero de Serie: ${numero_serie}`
+    );
+
+    //Querys
+    queryBuscarOrden = `SELECT * FROM trabajos WHERE nrocompro LIKE "ORX0011%${orden_reparacion}"`;
+    queryBuscarArticulo = `SELECT * FROM articulo WHERE codigo = ${codigo_articulo}`;
+    queryBuscarSerie = `SELECT * FROM serie2 WHERE serie = "${numero_serie}"`;
+
+    form.mensaje = "";
+    form.error = "";
+    form.ordenReparacion = orden_reparacion;
+    form.codigoArticulo = codigo_articulo;
+    form.numeroSerie = numero_serie;
+
+    let orden = await getFromUrbano(queryBuscarOrden);
+    if (orden.length === 0) {
+      form.error = "No existe numero de orden!!!";
+      form.mensaje = "";
+      form.orden = "";
+      form.articulo = "";
+
+      res.status(200).send({
+        usuario: { nombre: codigo_tecnico },
+        titulo: "Ingresar Articulo",
+        form: form,
+      });
+    } else {
+      let articulo = await getFromUrbano(queryBuscarArticulo);
+      if (articulo.length === 0) {
+        form.error = "No existe codigo de articulo!!!";
+        form.mensaje = "";
+        form.orden = "";
+        form.articulo = "";
+
+        res.status(200).send({
+          usuario: { nombre: codigo_tecnico },
+          titulo: "Ingresar Articulo",
+          form: form,
+        });
+      } else {
+        if (articulo[0].trabaserie === "N") {
+          form.mensaje = "Se ingreso articulo. Articulo no necesita serie.";
+          form.error = "";
+          form.ordenReparacion = "";
+          form.codigoArticulo = "";
+          form.numeroSerie = "";
+          form.orden = orden[0];
+          form.articulo = articulo[0];
+
+          res.status(200).send({
+            usuario: { nombre: codigo_tecnico },
+            titulo: "Ingresar Articulo",
+            form: form,
+          });
+        } else {
+          if (form.numeroSerie === "") {
+            form.error = "Debe ingresar numero de serie";
+            form.mensaje = "";
+            form.orden = "";
+            form.articulo = "";
+
+            res.status(200).send({
+              usuario: { nombre: codigo_tecnico },
+              titulo: "Ingresar Articulo",
+              form: form,
+            });
+          } else {
+            let articuloSerie = await getFromUrbano(queryBuscarSerie);
+            if (articuloSerie.length === 0) {
+              form.mensaje = "Se ingreso articulo!!! No se encontro serie";
+              form.error = "";
+              form.ordenReparacion = "";
+              form.codigoArticulo = "";
+              form.numeroSerie = "";
+              form.orden = orden[0];
+              form.articulo = articulo[0];
+
+              res.status(200).send({
+                usuario: { nombre: codigo_tecnico },
+                titulo: "Ingresar Articulo",
+                form: form,
+              });
+            } else {
+              if (articuloSerie[0].codigo === form.codigoArticulo) {
+                form.mensaje =
+                  "Se ingreso articulo, Serie coincide con articulo";
+                form.error = "";
+                form.ordenReparacion = "";
+                form.codigoArticulo = "";
+                form.numeroSerie = "";
+                form.orden = orden[0];
+                form.articulo = articulo[0];
+
+                res.status(200).send({
+                  usuario: { nombre: codigo_tecnico },
+                  titulo: "Ingresar Articulo",
+                  form: form,
+                });
+              } else {
+                form.error = "Serie pertenece a otro articulo!!!";
+                form.mensaje = "";
+                form.orden = "";
+                form.articulo = "";
+
+                res.status(200).send({
+                  usuario: { nombre: codigo_tecnico },
+                  titulo: "Ingresar Articulo",
+                  form: form,
+                });
+              }
+            }
+          }
+        }
+      }
+    }
+  } catch (error) {
+    logger.error(
+      `ingresarArticuloOrdenPost - Usuario: ${req.body.codigo_tecnico} - Error: ${error.message} - Host: ${req.body.host}`
+    );
+  }
+};
+
+exports.ingresarArticuloOrdenConfirmar = async (req, res) => {
+  try {
+    const host = req.body.host;
+    const codigo_tecnico = req.body.codigo_tecnico;
+    const tecnico = req.body.tecnico;
+    const orden_reparacion = req.body.ordenReparacion;
+    const codigo_articulo = req.body.codigoArticulo;
+    const numero_serie = req.body.numeroSerie;
+    queryBuscarOrden = `SELECT * FROM trabajos WHERE nrocompro LIKE "ORX0011%${orden_reparacion}"`;
+    let orden = await getFromUrbano(queryBuscarOrden);
+    const codigo_cliente = orden[0].cliente;
+
+    logger.info(
+      `ingresarArticuloOrdenConfirmar - Usuario: ${codigo_tecnico} - Host: ${host} 
+      Tecnico: ${tecnico}
+      Orden: ${orden_reparacion}
+      Codigo Articulo: ${codigo_articulo}
+      Numero de Serie: ${numero_serie}`
+    );
+
+    queryReservaArticulo = `UPDATE artstk01 SET reserd01 = reserd01 +1 WHERE codigo = ${codigo_articulo}`;
+    queryIngresarArticuloOrden = `
+        INSERT INTO trrenglo
+        (serie, ingreso, cliente, operador, tecnico, codart, descart, nrocompro, pendiente)
+        SELECT "${numero_serie}", NOW(), ${codigo_cliente}, "${codigo_tecnico}", "${tecnico}", ${codigo_articulo}, descrip, "ORX0011000${orden_reparacion}", 1 
+        FROM articulo
+        WHERE codigo = ${codigo_articulo}`;
+
+    setTimeout(() => {
+      res.status(200).send({
+        usuario: { nombre: codigo_tecnico },
+        titulo: "Ingresar Articulo",
+        confirmacion: "Articulo Ingresado",
+      });
+    }, 2000);
+  } catch (error) {
+    logger.error(
+      `ingresarArticuloOrdenConfirmar - Usuario: ${req.body.codigo_tecnico} - Error: ${error.message} - Host: ${req.body.host}`
     );
   }
 };
