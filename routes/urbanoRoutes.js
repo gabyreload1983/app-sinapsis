0db2762e62421158a1c4c21a292dfcf8ac64640a
Gabriel Sinapsis <gaby@sinapsissl.com>
2022-02-06 15:51:40 -0300

    Se agregan routes para ingresar articulo en orden

--- routes/urbanoRoutes.js
+++ routes/urbanoRoutes.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const urbano = require("../controllers/urbanoControllers");
-// const authController = require("../controllers/authControllers");
 const { requireAuth } = require("../middleware/auth");
 const admin = require("../middleware/admin");
 
@@ -10,8 +9,15 @@ router.get("/", urbano.index);
 router.get("/taller/ordenReparacion", urbano.ordenDeReparacion);
 router.get("/taller/ordenes-en-proceso", urbano.ordenesEnProceso);
 router.get("/taller/estadisticas-tecnicos", urbano.estadisticasTecnicos);
+router.get("/taller/ingresar-articulo-orden", urbano.ingresarArticuloOrdenGet);
+router.post(
+  "/taller/ingresar-articulo-orden-buscar",
+  urbano.ingresarArticuloOrdenBuscar
+);
+router.post(
+  "/taller/ingresar-articulo-orden-confirmar",
+  urbano.ingresarArticuloOrdenConfirmar
+);
 router.get("/taller/:sector", urbano.ordenesPendientes);
-/* router.get("/urbano/taller/estadisticas", getEstadisticas);
-router.get("/urbano/taller/estadisticas/:inicio/:final", getEstadisticas); */
 
 module.exports = router;
