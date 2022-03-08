27e9a0fb90c7575f3c6a53429550119cae7ee76f
sinapsisgaby <gaby@sinapsissl.com>
2022-03-08 15:08:06 -0300

    Se agrego comentario de prueba

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set("view engine", "ejs");
 //Environment
 logger.info(`NODE_ENV: ${process.env.NODE_ENV}`);
 logger.info(`EXPRESS: ${app.get("env")}`);
+//comment
 
 //Se chequea variables de entorno
 if (!config.get("mongoDb")) {
