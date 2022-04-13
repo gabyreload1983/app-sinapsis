8cb94a136c4eabd94b1e8b336fa9fe20b62fb40f
sinapsisgaby <gaby@sinapsissl.com>
2022-04-13 14:36:53 -0300

    Se borraron archivos que no se usan
    archivos js y ejs para ingresar y devolver articulos de orden.

--- public/js/articulo_seleccionado.js
+++
@@ -1,7 +0,0 @@
-function articulo_seleccionado(codigo, descripcion) {
-  const codigo_articulo = $("#codigo_articulo");
-  const descripcion_articulo = $(".descripcion_articulo");
-  codigo_articulo.val(codigo);
-  descripcion_articulo.empty(9);
-  descripcion_articulo.append(descripcion);
-}
