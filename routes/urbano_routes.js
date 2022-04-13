455708de86c74adfcad361ab9c9e1cf255bf3d4e
sinapsisgaby <gaby@sinapsissl.com>
2022-04-13 14:31:29 -0300

    Ingreso Devolucion Articulos
    Se quitaron rutas y controladores de logica vieja para ingresar y devolver articulos en ordenes.

--- routes/urbano_routes.js
+++ routes/urbano_routes.js
@@ -10,28 +10,13 @@ router.get("/taller/orden-reparacion", urbano.orden_de_reparacion);
 router.get("/taller/ordenes-en-proceso", urbano.ordenes_en_proceso);
 router.get("/taller/mis-ordenes-tomadas", urbano.mis_ordenes_tomadas);
 router.get("/taller/estadisticas-tecnicos", urbano.estadisticas_tecnicos);
-router.get(
-  "/taller/ingresar-articulo-orden",
-  urbano.ingresar_articulo_orden_get
-);
 router.get("/taller/ordenes-para-retirar", urbano.ordenes_para_retirar);
+router.get("/taller/:sector", urbano.ordenes_pendientes);
+
 router.post(
   "/taller/guardar-diagnostico-orden",
   urbano.guardar_diagnostico_orden
 );
-router.post(
-  "/taller/ingresar-articulo-orden-buscar",
-  urbano.ingresar_articulo_orden_buscar
-);
-router.post(
-  "/taller/ingresar-articulo-orden-confirmar",
-  urbano.ingresar_articulo_orden_confirmar
-);
-router.post("/taller/buscar-orden", urbano.buscar_orden_reparacion);
-router.post("/taller/buscar-articulo", urbano.buscar_articulo);
-router.post("/taller/buscar-articulo-serie", urbano.buscar_articulo_serie);
 router.post("/taller/tomar-orden", urbano.tomar_orden);
-
-router.get("/taller/:sector", urbano.ordenes_pendientes);
 
 module.exports = router;
